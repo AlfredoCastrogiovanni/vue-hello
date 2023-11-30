3aff0ec313dea9cc3ceca3547e6c101990771cd7
AlfredoCastrogiovanni <castro.alfry@gmail.com>
2023-11-30 15:16:56 +0100

    modifica attributo alt

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello!',
+            message: 'Super Alpaca',
             source: "https://www.repstatic.it/content/localirep/img/rep-firenze/2022/11/23/133706325-4e37649c-e126-4b55-949d-b46761970ef5.jpg"
         }
     }
